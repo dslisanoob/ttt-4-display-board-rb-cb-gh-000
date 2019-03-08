25fa1a165558ce9b61448f7cea010052a2ab64d5
Dhruv Gargi <dhruv.gargi@learner.manipal.edu>
2019-03-08 12:27:47 +0000

    Automatically backed up by Learn

--- spec/display_board_spec.rb
+++ spec/display_board_spec.rb
@@ -198,7 +198,7 @@ describe "#display_board in 'lib/display_board.rb" do
 
       # *** Edit the lines below ***
       # *** Uncomment the lines below ***
-         expect(rows[0]).to eq("  O | O | O ")
+         expect(rows[0]).to eq(" O | O | O ")
          expect(rows[1]).to eq("-----------")
          expect(rows[2]).to eq(" O | O | O ")
          expect(rows[3]).to eq("-----------")
